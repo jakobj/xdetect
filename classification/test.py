5136a8d13846411f7312bfbc74e59fa615845e38
Jakob Jordan <jakobjordan@posteo.de>
2021-03-03 23:02:51 +0100

    Support annotation of missclassifications in training images

--- classification/test.py
+++ classification/test.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
 import numpy as np
 from skimage import io
 import skimage.transform
 import torch
 from torch.utils.data import DataLoader
 import torchvision
-
+import sys
 
 from classifier import ConvNet
 from train import normalize, validation
@@ -13,6 +14,9 @@ from swissimage_10cm_dataset import SWISSIMAGE10cmDataset
 
 
 from train import MINIMAL_EDGE_LENGTH
+
+sys.path.insert(0, '../annotation/')
+from annotate import asset_from_file_name, mkdirp, save_patch
 
 
 class SegmentedImage(torch.utils.data.Dataset):
@@ -98,18 +102,39 @@ def create_heatmap(img, *, threshold=0.5):
     return heatmap
 
 
+def store_missclassified_locations(missclassifications_dir, *, img, asset, missclassified_locations):
+    mkdirp(missclassifications_dir)
+    for missclassified_loc_i in missclassified_locations:
+        save_patch(img[missclassified_loc_i[0]:missclassified_loc_i[2], missclassified_loc_i[1]:missclassified_loc_i[3]], output_dir=missclassifications_dir, asset=asset, bbox=missclassified_loc_i)
+
+
 if __name__ == '__main__':
 
-    # fn_test_image = "../data/swissimage-dop10_2018_2599-1198_0.1_2056.tif"
-    fn_test_image = "../data/swissimage-dop10_2018_2600-1200_0.1_2056.tif"
+    missclassifications_dir = "../data_annotated_50px/missclassified/"
+    fn_test_image = "../data/swissimage-dop10_2018_2599-1198_0.1_2056.tif"
+    # fn_test_image = "../data/swissimage-dop10_2018_2600-1200_0.1_2056.tif"
     img = io.imread(fn_test_image)
+    # img = img[:1000, :1000]
 
     heatmap = create_heatmap(img, threshold=0.99)
     assert img.shape[:2] == heatmap.shape
 
+    ax_ref = [None]
+    missclassified_locations = []
+    def onclick(event):
+        y = int(event.ydata // MINIMAL_EDGE_LENGTH) * MINIMAL_EDGE_LENGTH
+        x = int(event.xdata // MINIMAL_EDGE_LENGTH) * MINIMAL_EDGE_LENGTH
+        missclassified_locations.append((y, x, y + MINIMAL_EDGE_LENGTH, x + MINIMAL_EDGE_LENGTH))
+        ax_ref[0].add_patch(Rectangle((x, y), MINIMAL_EDGE_LENGTH, MINIMAL_EDGE_LENGTH, color='b', alpha=0.4))
+        event.canvas.draw_idle()
+
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_axes([0., 0., 1., 1.])
+    ax_ref[0] = ax
     ax.imshow(img)
     ax.imshow(heatmap, alpha=0.4, cmap='Reds', vmin=0, vmax=1)
     # plt.savefig('test.png', dpi=1200)
+    fig.canvas.mpl_connect("button_press_event", onclick)
     plt.show()
+
+    store_missclassified_locations(missclassifications_dir, img=img, asset=asset_from_file_name(fn_test_image), missclassified_locations=missclassified_locations)
