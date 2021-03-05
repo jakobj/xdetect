9914cbc419a76b846e42774173420792c22e6794
Jakob Jordan <jakobjordan@posteo.de>
2021-03-05 12:27:47 +0100

    Fix generation of negative examples

--- annotation/generate_negative_examples.py
+++ annotation/generate_negative_examples.py
@@ -4,15 +4,12 @@ import os
 from skimage import io
 
 from annotate import (
-    MINIMAL_EDGE_LENGTH,
     asset_from_filename,
     asset_prefix_from_asset,
     save_patch,
     mkdirp,
 )
-
-
-REL_NUMBER_NEGATIVE_EXAMPLES = 2
+import config
 
 
 def count_examples(*, examples_dir, asset_prefix):
@@ -38,29 +35,30 @@ def generate_negative_examples(*, asset_dir, examples_dir):
             examples_dir=os.path.join(examples_dir, "negative"),
             asset_prefix=asset_prefix,
         )
-        if n_negative_examples > REL_NUMBER_NEGATIVE_EXAMPLES * n_positive_examples:
+        if n_negative_examples > config.REL_NUMBER_NEGATIVE_EXAMPLES * n_positive_examples:
             raise RuntimeError(
                 f"too many negative examples for asset '{asset}' - remove a few"
             )
 
         img = io.imread(fn)
 
-        n_rows = img.shape[0] // MINIMAL_EDGE_LENGTH
-        n_cols = img.shape[1] // MINIMAL_EDGE_LENGTH
+        n_rows = img.shape[0] // config.MINIMAL_EDGE_LENGTH
+        n_cols = img.shape[1] // config.MINIMAL_EDGE_LENGTH
 
-        assert n_rows * MINIMAL_EDGE_LENGTH == img.shape[0]
-        assert n_cols * MINIMAL_EDGE_LENGTH == img.shape[1]
+        assert n_rows * config.MINIMAL_EDGE_LENGTH == img.shape[0]
+        assert n_cols * config.MINIMAL_EDGE_LENGTH == img.shape[1]
 
-        while n_negative_examples < REL_NUMBER_NEGATIVE_EXAMPLES * n_positive_examples:
+        print(f"  generating negative examples for asset '{asset}'")
+        while n_negative_examples < config.REL_NUMBER_NEGATIVE_EXAMPLES * n_positive_examples:
 
             row = np.random.randint(n_rows)
             col = np.random.randint(n_cols)
 
             bbox = (
-                row * MINIMAL_EDGE_LENGTH,
-                col * MINIMAL_EDGE_LENGTH,
-                (row + 1) * MINIMAL_EDGE_LENGTH,
-                (col + 1) * MINIMAL_EDGE_LENGTH,
+                row * config.MINIMAL_EDGE_LENGTH,
+                col * config.MINIMAL_EDGE_LENGTH,
+                (row + 1) * config.MINIMAL_EDGE_LENGTH,
+                (col + 1) * config.MINIMAL_EDGE_LENGTH,
             )
             patch = img[bbox[0] : bbox[2], bbox[1] : bbox[3]]
 
@@ -75,7 +73,7 @@ def generate_negative_examples(*, asset_dir, examples_dir):
 
 
 if __name__ == "__main__":
-    asset_dir = "../data/"
-    examples_dir = f"../data_annotated_{MINIMAL_EDGE_LENGTH}px/"
+    asset_dir = "../data/assets/"
+    examples_dir = f"../data/crosswalks/"
 
     generate_negative_examples(asset_dir=asset_dir, examples_dir=examples_dir)
