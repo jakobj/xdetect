9c371068bfc1b77d32a03081c55426d95c682e26
Jakob Jordan <jakobjordan@posteo.de>
2021-03-05 19:33:35 +0100

    Support accumulation of intersecting ROIs

---
+++ conversion/accumulate_ROIs.py
@@ -0,0 +1,52 @@
+import fiona
+import geopandas as gpd
+import glob
+import matplotlib.pyplot as plt
+import os
+import shapely.ops
+
+
+def merge_two_polygons(polygons):
+    for i, poly_i in enumerate(polygons):
+        for j, poly_j in enumerate(polygons):
+            if i != j:
+                if poly_i.intersects(poly_j):
+                    if i < j:
+                        del polygons[j]
+                        del polygons[i]
+                    else:
+                        del polygons[i]
+                        del polygons[j]
+                    polygons.append(shapely.ops.unary_union([poly_i, poly_j]))
+                    return polygons, True
+
+    return polygons, False
+
+
+def merge_polyons(df):
+    polygons = list(df['geometry'])
+
+    polygons, changed = merge_two_polygons(polygons)
+    while changed:
+        polygons, changed = merge_two_polygons(polygons)
+
+    return gpd.GeoDataFrame(geometry=gpd.GeoSeries(polygons))
+
+
+if __name__ == '__main__':
+
+    export_dir = "../data/export/"
+
+    for fn in glob.glob(os.path.join(export_dir, "ROIs-swissimage-*.geojson")):
+
+        print(f"  processing {fn}")
+
+        try:
+            df = gpd.read_file(fn)
+        except fiona.errors.DriverError:
+            continue
+
+        df_merged = merge_polyons(df)
+
+        fn_prefix = os.path.splitext(os.path.basename(fn))[0]
+        df_merged.to_file(os.path.join(export_dir, f"{fn_prefix}-merged.geojson"), driver='GeoJSON')
--- conversion/export_kml.py
+++ conversion/export_kml.py
@@ -8,18 +8,17 @@ gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
 
 if __name__ == '__main__':
 
-    export_dir = "../data_exported/"
+    export_dir = "../data/export/"
     label = 'crosswalks'
 
     points = gpd.GeoSeries()
-    for fn in glob.glob(os.path.join(export_dir, "ROIs-swissimage*.geojson")):
-        prefix = os.path.splitext(os.path.basename(fn))[0]
-
+    for fn in glob.glob(os.path.join(export_dir, "ROIs-swissimage*-merged.geojson")):
         try:
-            gdf = gpd.read_file(os.path.join(export_dir, fn))
+            gdf = gpd.read_file(fn)
         except fiona.errors.DriverError:
             continue
 
-        points = points.append(gdf['geometry'].centroid, ignore_index=True)
+        # points = points.append(gdf['geometry'].centroid, ignore_index=True)
+        points = points.append(gdf['geometry'], ignore_index=True)
 
     gpd.GeoDataFrame(geometry=points).to_file(os.path.join(export_dir, f"POIs-{label}.kml"), driver='KML')
